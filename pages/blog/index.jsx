0fbff8f0336fc267867d77098a3c953ddc16402f
furbox <furbox@gmail.com>
2021-07-30 17:41:19 -0500

    getStaticProps

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,9 +1,31 @@
 import Layout from "../../components/Layout";
 
-export default function index() {
+export default function index({ data }) {
     return (
         <Layout>
             <h1>my blog</h1>
+            {
+                data.map(({ id, title, body }) => (
+                    <div key={id}>
+                        <h3>{id} - {title}</h3>
+                        <p>{body}</p>
+                    </div>
+                ))
+            }
         </Layout>
     )
+}
+
+export async function getStaticProps() {
+    try {
+        const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const data = await res.json();
+        return {
+            props: {
+                data
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    }
 }
